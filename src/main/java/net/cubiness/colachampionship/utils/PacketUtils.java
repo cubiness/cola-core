b153853a48a3743fac32936d9a6c00892d6ab23e
Neil Macneale <neil.macneale.v@gmail.com>
2020-07-02 15:57:46 -0700

    Removed setCustomName from PacketUtils, as I couldn't get it to work

--- src/main/java/net/cubiness/colachampionship/utils/PacketUtils.java
+++ src/main/java/net/cubiness/colachampionship/utils/PacketUtils.java
@@ -1,14 +1,6 @@
 package net.cubiness.colachampionship.utils;
 
-import com.comphenix.protocol.PacketType.Play.Server;
 import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Optional;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 
 public class PacketUtils {
 
@@ -17,25 +9,4 @@ public class PacketUtils {
   public PacketUtils(ProtocolManager manager) {
     this.manager = manager;
   }
-
-  /**
-   * Sets the custom name of an entity for one player
-   * @param p The player the name should be displayed to
-   * @param e The entity to change the name of
-   * @param name the new name
-   */
-  public void setCustomName(Player p, Entity e, String name) {
-    PacketContainer packet = new PacketContainer(Server.ENTITY_METADATA);
-    packet.getIntegers().write(0, e.getEntityId());
-
-    WrappedDataWatcher metadata = WrappedDataWatcher.getEntityWatcher(e);
-    metadata.setObject(2, Optional.of(WrappedChatComponent.fromText(name).getHandle()));
-    packet.getWatchableCollectionModifier().write(0, metadata.getWatchableObjects());
-    try {
-      manager.sendServerPacket(p, packet);
-    } catch (InvocationTargetException err) {
-      throw new RuntimeException("Cannot send packet " + packet, err);
-    }
-  }
-
 }
