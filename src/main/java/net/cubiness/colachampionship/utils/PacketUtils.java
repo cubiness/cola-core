49b4ff07770da9abb1ba474319a0fe51bf5f96da
Neil Macneale <neil.macneale.v@gmail.com>
2020-07-04 14:53:16 -0700

    Added spawn armor stand function to packet utils

--- src/main/java/net/cubiness/colachampionship/utils/PacketUtils.java
+++ src/main/java/net/cubiness/colachampionship/utils/PacketUtils.java
@@ -1,6 +1,13 @@
 package net.cubiness.colachampionship.utils;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import com.comphenix.protocol.PacketType.Play.Server;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
 
 public class PacketUtils {
 
@@ -8,5 +15,25 @@ public class PacketUtils {
 
   public PacketUtils(ProtocolManager manager) {
     this.manager = manager;
+  }
+
+  public int spawnArmorStand(Player p, Location l) {
+    PacketContainer packet = new PacketContainer(Server.SPAWN_ENTITY_LIVING);
+
+    int id = (int) (Math.random() * Integer.MAX_VALUE);
+
+    packet.getModifier().writeDefaults();
+    packet.getIntegers().write(0, id); // id of entity
+    packet.getIntegers().write(1, 1); // type id of armor stand
+    packet.getDoubles().write(0, l.getX());
+    packet.getDoubles().write(1, l.getY());
+    packet.getDoubles().write(2, l.getZ());
+
+    try {
+      manager.sendServerPacket(p, packet);
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
+    return id;
   }
 }
